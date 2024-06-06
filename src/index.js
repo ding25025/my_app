448b20698836ac6dd07c66b2723ff1eb9bba8723
yutingwu <ding25025@gmail.com>
2024-06-06 11:16:13 +0800

    feat(App.js,index.js): add google login function

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 const CLIENT_ID = process.env.REACT_APP_GOOGLE_CLIENT_ID;
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <GoogleOAuthProvider clientId={CLIENT_ID}>
     <React.StrictMode>
